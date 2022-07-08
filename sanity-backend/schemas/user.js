a6d7493897c8ab8f9a19c081a19206bade7f2e2f
Yoosuf Sayyid <cena26657@gmail.com>
2022-07-08 14:18:26 +0500

    f

---
+++ sanity-backend/schemas/user.js
@@ -0,0 +1,19 @@
+export default {
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields:[
+        {
+            name: 'UserName',
+            title: 'User Name',
+            type: 'string',
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string',
+        }
+
+    ]
+
+}
