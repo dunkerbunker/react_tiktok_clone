b34bd8d186da2ca28cfc46c9e6c28ffad263d733
Yoosuf Sayyid <cena26657@gmail.com>
2022-07-14 09:47:28 +0500

    h

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -12,7 +12,6 @@ const Discover = () => {
     const { topic } = router.query;
 
     const activeTopicStyle = 'xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#F51997]';
-    
     const topicStyle = 'xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black';
 
     return (
