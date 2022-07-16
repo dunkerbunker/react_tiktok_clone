9b300768c701f0cdb1f13c1378676e8fb6433d3b
Yoosuf Sayyid <cena26657@gmail.com>
2022-07-16 10:47:33 +0500

    login logout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,10 +8,14 @@ import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
 
 import Logo from '../utils/tiktik-logo.png';
+import { createOrGetUser } from '../utils';
+
+import useAuthStore from '../store/authStore';
 
 const Navbar = () => {
 
   const user = false;
+  const { userProfile, addUser, removeUser } = useAuthStore();
 
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
@@ -26,11 +30,42 @@ const Navbar = () => {
       </div>
 
       <div>
-        {user ? (
-          <div>Logged In</div>
+        {userProfile ? (
+          <div className="flex gap-5 md:gap-10">
+            <Link href="/upload">
+              <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
+                <IoMdAdd className='text-xl' /> {`  `}
+                <span className='hidden md:block'>Upload</span>
+              </button>
+            </Link>
+            {userProfile.image && (
+              <Link href="/">
+              <>
+                <Image
+                  width={40}
+                  height={40}
+                  className=' rounded-full cursor-pointer'
+                  src={userProfile.image}
+                  alt='user-profile'
+                />
+              </>
+            </Link>
+            )}
+            <button
+                type='button'
+                className=' border-2 p-2 rounded-full cursor-pointer outline-none'
+                onClick={() => {
+                  googleLogout();
+                  removeUser();
+                }}
+              >
+                <AiOutlineLogout color='red' fontSize={21} />
+              </button>
+          </div>
+
         ) : (
           <GoogleLogin 
-            onSuccess={(response) => console.log(response)}
+            onSuccess={(response) => createOrGetUser(response, addUser)}
             onError={() => console.log(Error)}
           />
         )}
