af2f38f88c8162b704ca0cc6c44ccffb21fc2f67
Yoosuf Sayyid <cena26657@gmail.com>
2022-07-19 18:54:48 +0500

    y

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -15,7 +15,17 @@ import useAuthStore from '../store/authStore';
 const Navbar = () => {
 
   const user = false;
+  const [searchValue, setSearchValue] = useState('');
+  const router = useRouter();
   const { userProfile, addUser, removeUser } = useAuthStore();
+
+  const handleSearch = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    
+    if(searchValue) {
+      router.push(`/search/${searchValue}`);
+    }
+  };
 
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
@@ -25,8 +35,24 @@ const Navbar = () => {
         </div>
       </Link>
 
-      <div>
-        Search
+      <div className='relative hidden md:block'>
+        <form
+          onSubmit={handleSearch}
+          className='absolute md:static top-10 -left-20 bg-white'
+        >
+          <input
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            className='bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full  md:top-0'
+            placeholder='Search accounts and videos'
+          />
+          <button
+            onClick={handleSearch}
+            className='absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400'
+          >
+            <BiSearch />
+          </button>
+        </form>
       </div>
 
       <div>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,23 +2,30 @@ import React, { useState } from 'react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import { GoogleLogin } from 'react-google-login';
+import { useGoogleLogin } from '@react-oauth/google';
 import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
 import { ImCancelCircle } from 'react-icons/im';
-
 
 import Discover from './Discover';
 import SuggestedAccounts from './SuggestedAccounts';
 import Footer from './Footer';
+import { createOrGetUser } from '../utils';
+import useAuthStore from '../store/authStore';
 
 
 const Sidebar = () => {
 
+  const user = false;
+
+  const { userProfile, addUser, removeUser } = useAuthStore();
+
   const [showSidebar, setShowSidebar] = useState(true);
 
-  const userProfile = false;
-
   const normalLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl-justify-start cursor-pointer font-semibold text-[#F51997] rounded';
+
+  const login = useGoogleLogin({
+    onSuccess: response => createOrGetUser(response, addUser),
+  });
 
   return (
     <div>
@@ -41,25 +48,14 @@ const Sidebar = () => {
               </div>
             </Link>
           </div>
+
           {!userProfile && (
             <div className='px-2 py-4 hidden xl:block'>
               <p className='text-gray-400'>Login to like and comment on videos</p>
               <div className="pr-4">
-                <GoogleLogin 
-                  clientId=""
-                  render={(renderProps) => (
-                    <button
-                      className='cursor pointer bg-white text-lg text-[#F51997] border-[1px] border-[#F51997] font-semibold rounded-md px-6 py-3 outline-none w-full mt-3 hover:text-white hover:bg-[#F51997]'
-                      onClick={renderProps.onClick}
-                      disabled={renderProps.disabled}
-                    >
-                      Log In
-                    </button>
-                  )}
-                  onSuccess={() => {}}
-                  onFailure={() => {}}
-                  cookiePolicy='single_host_origin'
-                />
+                <button className='cursor pointer bg-white text-lg text-[#F51997] border-[1px] border-[#F51997] font-semibold rounded-md px-6 py-3 outline-none w-full mt-3 hover:text-white hover:bg-[#F51997]' onClick={() => login()} >
+                  Login
+                </button>
               </div>
             </div>
           )}
