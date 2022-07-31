56513af8ba69a684f5de99d0bbadd3ac948f90d4
Yoosuf Sayyid <cena26657@gmail.com>
2022-07-31 20:15:59 +0500

    kk

--- pages/api/search/[searchTerm].ts
+++ pages/api/search/[searchTerm].ts
@@ -12,5 +12,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const videos = await client.fetch(videosQuery);
 
     res.status(200).json(videos);
-  }
-}
+  
+
