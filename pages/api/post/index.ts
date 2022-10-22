cb18a8c3ceb3ecd2d6449616de8ed22b43d0262f
Yoosuf Sayyid <cena26657@gmail.com>
2022-10-22 11:46:30 +0500

    reee

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -6,6 +6,7 @@ import { client } from '../../../utils/client';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     const query = allPostsQuery();
+
     const data = await client.fetch(query);
 
     res.status(200).json(data);
